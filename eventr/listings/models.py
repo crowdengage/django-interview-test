3eb3f17820f84aca4954d646aa732350edd72426
Hugh Topping <hugh@hugh.io>
2021-07-26 14:43:00 +0100

    Adds starter models for listings app

--- eventr/listings/models.py
+++ eventr/listings/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Source(models.Model):
+    spektrix_client_name = models.TextField(unique=True)
+    friendly_name = models.TextField()
+
+class Event(models.Model):
+    source = models.ForeignKey(Source, related_name="events", on_delete=models.CASCADE)
+    name = models.TextField()
+    # TODO Which other fields should we store for an Event?
+
+class EventInstance(models.Model):
+    event = models.ForeignKey(Event, related_name="instances", on_delete=models.CASCADE)
+    start_datetime = models.DateTimeField()
+    # TODO Which other fields should we store for an EventInstance?
